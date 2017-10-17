d4090eab79c839b6ce598030db17efeb640a831d
Maksym Havryliuk <maks.havryliuk@gmail.com>
2017-10-17 13:28:13 +0300

    Написал решение задания 3 по теме Классы и объекты

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ClassesAndObjectsTask3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Rectangle firstRectangle = new Rectangle();
+
+            Console.ReadKey();
+        }
+    }
+}
